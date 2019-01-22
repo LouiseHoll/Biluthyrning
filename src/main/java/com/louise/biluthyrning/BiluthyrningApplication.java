3c3dcdd9bad98aa1253a78e6c8de472660fb7621
LouiseHoll <louise.hollingworth@gmail.com>
2019-01-22 10:35:23 +0100

    grund

---
+++ src/main/java/com/louise/biluthyrning/BiluthyrningApplication.java
@@ -0,0 +1,14 @@
+package com.louise.biluthyrning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiluthyrningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiluthyrningApplication.class, args);
+	}
+
+}
+
